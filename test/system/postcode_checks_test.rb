ce2e7bdd0ba0a57c5cdd85665f574c64e14f0a0c
Laurent Curau <laurent@thecodeshed.dev>
2020-12-05 16:44:47 +0100

    Stub requests to Postcodes API

--- test/system/postcode_checks_test.rb
+++ test/system/postcode_checks_test.rb
@@ -1,19 +1,39 @@
 require "application_system_test_case"
 
 class PostcodeChecksTest < ApplicationSystemTestCase
-   test "allowed postcode" do
-     visit root_url
-     fill_in "Postcode", with: "SE1 7QD"
-     click_on "Check"
+  test "allowed postcode" do
+    response = {
+      "status": 200,
+      "result": {
+        "lsoa": "Southwark 034A"
+      }
+    }.to_json
 
-     assert_text "Postcode is allowed"
-   end
+    stub_request(:get, "https://api.postcodes.io/postcodes/foo").
+      to_return(body: response)
 
-   test "prohibited postcode" do
-     visit root_url
-     fill_in "Postcode", with: "AAA BBB"
-     click_on "Check"
+    visit root_url
+    fill_in "Postcode", with: "foo"
+    click_on "Check"
 
-     assert_text "Postcode is prohibited"
-   end
+    assert_text "Postcode is allowed"
+  end
+
+  test "prohibited postcode" do
+    response = {
+      "status": 200,
+      "result": {
+        "lsoa": "Barshire 034A"
+      }
+    }.to_json
+
+    stub_request(:get, "https://api.postcodes.io/postcodes/bar").
+      to_return(body: response)
+
+    visit root_url
+    fill_in "Postcode", with: "bar"
+    click_on "Check"
+
+    assert_text "Postcode is prohibited"
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'webmock/minitest'
+
+WebMock.allow_net_connect!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
